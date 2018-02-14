d3d6f4080b5e8c117dc4e90cff49bdad19c93620
Abhishek mutyampeta <abhi.abhishek594@gmail.com>
2018-02-14 12:15:47 -0600

    Update app.js

--- src/components/app.js
+++ src/components/app.js
@@ -7,6 +7,7 @@ import BodyBackgroundColor from 'react-body-backgroundcolor';
 export default class App extends Component {
   render() {
     return (
+      
       <BodyBackgroundColor backgroundColor='#FFFF00'>
       <div>
 
